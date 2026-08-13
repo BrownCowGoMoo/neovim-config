002d887a2a310cf50d2419e8b87e7f71f16ce7f2
Ian <browncow@moo.com>
2026-08-13 01:27:27 -0400

    Added keymaps making use of telescope

--- lua/browncow/telescope.lua
+++ lua/browncow/telescope.lua
@@ -4,5 +4,14 @@ do
 	vim.pack.add({'https://github.com/nvim-lua/plenary.nvim',
 	'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
 	'https://github.com/nvim-telescope/telescope.nvim'})
+
+	require('telescope').setup()
+
+	local tele = require('telescope.builtin')
+	vim.keymap.set('n', '<leader>sf', tele.find_files, { desc = 'find files' })
+	vim.keymap.set('n', '<leader>sg', tele.live_grep, { desc = 'live grep' })
+	vim.keymap.set('n', '<leader>sh', tele.help_tags, { desc = 'seach help' })
 end
+
+
 
