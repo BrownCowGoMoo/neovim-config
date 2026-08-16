466886d66a3d613c793cb2e07393f60e62cf4f31
Ian <browncow@moo.com>
2026-08-15 22:42:00 -0400

    refactored lsp.lua.  Consolidated tables do only one table is needed and streamlined logic.

--- lua/browncow/lsp/lsp.lua
+++ lua/browncow/lsp/lsp.lua
@@ -11,80 +11,57 @@ do
 		automatic_enable = false
 	})
 
-	local function is_within_value(query, intable)
-		for _, item in ipairs(intable) do
-			if query == item then
+	local function is_within(item, tbl)
+		for _, parser in ipairs(tbl) do
+			if item == parser then
 				return true
 			end
 		end
 		return false
 	end
 
-	local function is_within_keys(query, intable)
-		for item, _ in pairs(intable) do
-			if query == item then
-				return true
-			end
-		end
-		return false
-	end
-
-	local function attach_lsp(buffer, lang)
-		if not vim.treesitter.language.add(lang) then return end
-		vim.treesitter.start(buffer, lang)
-	end
-
-
-	local fname_pname = {
-		lua = 'lua_ls',
-		python = 'pyright',
-	}
-
-	local language_servers = {
-		lua_ls = {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { 'vim' },
+	local name_server_config = {
+		lua = {
+			server = 'lua_ls',
+			config = {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { 'vim' },
+						},
 					},
 				},
 			},
 		},
-		stylua = {},
-		pyright = {},
 	}
 
-	local function enable_language_server(server, language_servers)
-		vim.lsp.config(server, language_servers[server])
-		vim.lsp.enable(server)
-	end
-
-
-
-
-	local available_parsers = require('nvim-treesitter').get_available()
 	vim.api.nvim_create_autocmd('FileType', {
 		callback = function(event)
-
 			local filetype, buffer = event.match, event.buf
-			local lang = vim.treesitter.language.get_lang(filetype)
-			if not lang then
-				print(('No language parser was found for filetype: %s'):format(filetype))
+			local language = vim.treesitter.language.get_lang(filetype)
+			if not language then
+				print(('No language parser found for filetype: %s'):format(filetype))
 				return
 			end
-
 			local installed_parsers = require('nvim-treesitter').get_installed()
-			if is_within_value(lang, installed_parsers) then
-				attach_lsp(buffer, lang)
+			if is_within(language, installed_parsers) then
+				if not vim.treesitter.language.add(language) then return end
+				vim.treesitter.start(buffer, language)
 			end
-
-
-			if is_within_keys(lang, fname_pname) then
-				enable_language_server(fname_pname[lang], language_servers)
-			else
-				print(('No language server found for language: %s'):format(lang))
+			if not name_server_config[language] then
+				print(('No language %s found in the given table'):format(language))
+				return
 			end
+			if not name_server_config[language]['server'] then
+				print(('No config found in the given table for language %s'):format(language))
+				return
+			end
+			local server = name_server_config[language]['server']
+			local config = name_server_config[language]['config']
+			vim.lsp.config(server, config)
+			vim.lsp.enable(server)
 		end,
 	})
-
 end
+
+
