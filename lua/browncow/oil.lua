9f5821093313105587079d25e5784b80ce228419
Ian <browncow@moo.com>
2026-08-15 22:36:36 -0400

    added do end block

--- lua/browncow/oil.lua
+++ lua/browncow/oil.lua
@@ -1,8 +1,8 @@
+do
+	vim.pack.add({'https://github.com/stevearc/oil.nvim'})
+	require('oil').setup()
 
-vim.pack.add({'https://github.com/stevearc/oil.nvim'})
-require('oil').setup()
-
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'open parent directory' })
-
+	vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'open parent directory' })
+end
 
 
