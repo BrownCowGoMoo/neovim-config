4cbbac39e22b140a7309fbdd94d5dd7d4f4aeebb
Ian <browncow@moo.com>
2026-08-19 12:20:45 -0400

    Added the mini plugin, included mini.ai, mini.surround, and mini.icons

---
+++ lua/browncow/mini.lua
@@ -0,0 +1,22 @@
+do
+
+	vim.pack.add({'https://github.com/nvim-mini/mini.nvim'})
+
+	if vim.g.have_nerd_font then
+		require('mini.icons').setup()
+		MiniIcons.mock_nvim_web_devicons()
+	end
+
+	require('mini.ai').setup({
+		mappings = {
+			around_next = 'aa',
+			inside_next = 'ii',
+		},
+		n_lines = 500,
+	})
+
+	require('mini.surround').setup()
+end
+
+
+
