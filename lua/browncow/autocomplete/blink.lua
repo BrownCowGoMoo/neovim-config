e5bfe7242b0fdd44e127fb3c4f62ed258dd7cb62
Ian <browncow@moo.com>
2026-08-19 12:23:25 -0400

    Added blink.cmp and snippets plugins to enable autocomplete

---
+++ lua/browncow/autocomplete/blink.lua
@@ -0,0 +1,39 @@
+require('luasnip').setup({})
+
+vim.pack.add({
+	{
+		src = 'https://github.com/Saghen/blink.cmp',
+		version = vim.version.range('1.*'),
+	},
+})
+
+require('blink.cmp').setup({
+	keymap = {
+		preset = 'default',
+	},
+	appearance = {
+		nerd_font_variant = 'mono',
+	},
+	completion = {
+		documentation = {
+			auto_show = false,
+			auto_show_delay_ms = 500,
+		},
+	},
+	sources = {
+		default = {
+			'lsp',
+			'path',
+			'snippets',
+		},
+	},
+	snippets = {
+		preset = 'luasnip',
+	},
+	fuzzy = {
+		implementation = 'lua',
+	},
+	signature = {
+		enabled = true,
+	},
+})
